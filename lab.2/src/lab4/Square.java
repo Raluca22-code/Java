5b0c37543bc8b00b4e0b6418c487768d7c8b8287
ralucanemes <nemesra7uca555@gmail.com>
2022-03-14 11:37:09 +0200

    no message

---
+++ lab.2/src/lab4/Square.java
@@ -0,0 +1,35 @@
+package lab4;
+//the subclass of Rectangle
+
+public class Square extends Rectangle {
+    public Square() {
+        super();
+    }
+    public Square(double side){
+        super.setWidth(side);
+    }
+    public Square(double side, String color, boolean filled){
+        super.setWidth(side);
+        super.setColor(color);
+        super.setFilled(filled);
+
+    }
+    public double getSide(){
+        return super.getWidth();
+    }
+    public void setSide(double width){
+        super.setWidth(width);
+    }
+    @Override
+    public void setWidth(double width){
+        super.setWidth(width);
+    }
+    @Override
+    public void setLength(double length){
+        super.setLength(length);
+    }
+    @Override
+    public String toString() {
+        return "A square with side" + getSide() + "which is a subclass of " + super.toString();
+    }
+}
---
+++ lab.2/src/lab4/TestSquare.java
@@ -0,0 +1,12 @@
+package lab4;
+
+public class TestSquare {
+    public static void main(String[] args){
+        Square test1 = new Square(22,"green", true);
+        System.out.println("Side : " + test1.getSide());
+        System.out.println("Color : " + test1.getColor());
+        System.out.println("Color : " + test1);
+
+    }
+}
+
